fa2f1b9c2765646aab5f3d392a4a693ab37a4409
angryhussord <angryhussord@outlook.com>
2015-07-31 15:58:24 -0700

    initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'equalizerapo';
+$installerType = 'exe';
+$url = 'http://sourceforge.net/projects/equalizerapo/files/latest/download';
+$silentArgs = '/S';
+$validExitCodes = @(0);
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes;
