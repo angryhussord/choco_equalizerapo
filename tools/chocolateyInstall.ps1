e007ede969cdf96354af7e682b7909238c132882
Patrick Hufford <pahuff@microsoft.com>
2015-08-26 16:34:05 -0700

    Monitoring job
    Job setup to monitor for and kill Configurator.exe so the setup is
    non-interactive.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,25 @@
 ﻿$packageName = 'equalizerapo';
 $installerType = 'exe';
-$url = 'http://sourceforge.net/projects/equalizerapo/files/latest/download';
+$url = 'http://sourceforge.net/projects/equalizerapo/files/0.9.2/EqualizerAPO32-0.9.2.exe/download';
+$url64 = 'http://sourceforge.net/projects/equalizerapo/files/0.9.2/EqualizerAPO64-0.9.2.exe/download';
 $silentArgs = '/S';
 $validExitCodes = @(0);
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes;
+#Create monitor for "Configurator" ahead of actually installing the software
+$ScriptBlock = {
+	$setup_running = $true;
+	while ($setup_running) {
+		Sleep -Seconds 1;
+		$setup_running = (Get-Process 'EqualizerAPO*-0.9.2' -ErrorAction:SilentlyContinue).count -eq 1;
+	}
+	$configurator_running = $true;
+	while ($configurator_running) {
+		Sleep -Seconds 1;
+		Get-Process 'configurator' | Stop-Process;
+		$configurator_running = (Get-Process 'configurator' -ErrorAction:SilentlyContinue).count -eq 1;
+	}
+}
+Start-Job -Name "Kill Configurator.exe" -ScriptBlock $ScriptBlock | Out-Null;
+#Install the package
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes;
+#Cleanup the Job
+Remove-Job -Name "Kill Configurator.exe" -Force | Out-Null;
